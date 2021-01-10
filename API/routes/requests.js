aab92bb98992c80cd24ab30bf0352f6d85cfa38d
fernandojmsilva <nandojmsilva04@gmail.com>
2021-01-10 15:25:14 +0000

    logical_delete on

--- API/routes/requests.js
+++ API/routes/requests.js
@@ -7,7 +7,7 @@ router.get('/', requests.read);
 router.get('/:request_id', requests.readById);
 router.post("/", requests.save);
 router.put('/:request_id', requests.update);
-//router.put('/request/:request_id', requests.logicalDelete);  
+router.put('/request/:request_id', requests.logicalDelete);  
 router.delete('/:request_id', requests.deleteID);
 
 module.exports = router;
